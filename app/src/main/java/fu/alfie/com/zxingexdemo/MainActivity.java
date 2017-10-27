2e6e5f7aced89ac6fbd5493a2a21074253d88103
傅俊皓 <alfie.fu@toplogis.com>
2017-10-27 10:38:32 +0800

    使用Zxing官方提供的方法

--- app/src/main/java/fu/alfie/com/zxingexdemo/MainActivity.java
+++ app/src/main/java/fu/alfie/com/zxingexdemo/MainActivity.java
@@ -16,7 +16,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import fu.alfie.com.zxingexdemo.custom_view.CustomCaptureActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,66 +34,83 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////
-
+    /////////////////////////////////////////Zxing官方//////////////////////////////////////////
 
     public void onQRcodeScanClick(View view){
         new IntentIntegrator(this)
-//                .setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES) //只掃bar_code，不設定時什麼都掃
-                .setPrompt("Scan a barcode")//提示文字
-                .setCameraId(0)//後攝影機
-                .setBeepEnabled(false)//是否要有音效
-                .setBarcodeImageEnabled(true)//保留掃描成功的截圖
-                .setOrientationLocked(false)//鎖定方向
-                .setTimeout(5000)//若未掃描5秒後自動關閉
-                .initiateScan();//只留這個就可全部用預設
-    }
-
-    public void onCustomQRcodeScanClick(View view){
-        new IntentIntegrator(this)
-                .setCaptureActivity(CustomCaptureActivity.class) //使用客製化頁面
-                .setBarcodeImageEnabled(true)//保留掃描成功的截圖
-                .initiateScan();//只留這個就可全部用預設
+                .initiateScan(); //開啟外部掃描器
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
-                Toast.makeText(this, "掃描停止", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(this, "條碼內容: " + result.getContents(), Toast.LENGTH_LONG).show();
-                Toast.makeText(this, "條碼格式: " + result.getFormatName(), Toast.LENGTH_LONG).show();
-
-                //輸出圖片 方法一
-//                File imgFile = new  File(result.getBarcodeImagePath());
-//                if(imgFile.exists()){
-//                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
-//                    ImageView imageView = (ImageView) findViewById(R.id.imageView2);
-//                    imageView.setImageBitmap(myBitmap);
-//                }
-                //輸出圖片 方法二
-                showBarcodeImage(result.getBarcodeImagePath());
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) { //RESULT_OK = -1
+            IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            String scanResult = result.getContents();
+            Toast.makeText(MainActivity.this, "條碼內容: "+scanResult, Toast.LENGTH_LONG).show();
         }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
-    private void showBarcodeImage(String barcodeImagePath) {
-        FileInputStream fileInputStream = null;
-        try {
-            fileInputStream = new FileInputStream(new File(barcodeImagePath));
-            ((ImageView)findViewById(R.id.imageView2)).setImageBitmap(BitmapFactory.decodeStream(fileInputStream));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                fileInputStream.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+
+    /////////////////////////////////////////ZxingEmbedded//////////////////////////////////////////
+//
+//    public void onQRcodeScanClick(View view){
+//        new IntentIntegrator(this)
+////                .setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES) //只掃bar_code，不設定時什麼都掃
+//                .setPrompt("Scan a barcode")//提示文字
+//                .setCameraId(0)//後攝影機
+//                .setBeepEnabled(false)//是否要有音效
+//                .setBarcodeImageEnabled(true)//保留掃描成功的截圖
+//                .setOrientationLocked(false)//鎖定方向
+//                .setTimeout(5000)//若未掃描5秒後自動關閉
+//                .initiateScan();//只留這個就可全部用預設
+//    }
+//
+//    public void onCustomQRcodeScanClick(View view){
+//        new IntentIntegrator(this)
+//                .setCaptureActivity(CustomCaptureActivity.class) //使用客製化頁面
+//                .setBarcodeImageEnabled(true)//保留掃描成功的截圖
+//                .initiateScan();//只留這個就可全部用預設
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+//        if(result != null) {
+//            if(result.getContents() == null) {
+//                Toast.makeText(this, "掃描停止", Toast.LENGTH_LONG).show();
+//            } else {
+//                Toast.makeText(this, "條碼內容: " + result.getContents(), Toast.LENGTH_LONG).show();
+//                Toast.makeText(this, "條碼格式: " + result.getFormatName(), Toast.LENGTH_LONG).show();
+//
+//                //輸出圖片 方法一
+////                File imgFile = new  File(result.getBarcodeImagePath());
+////                if(imgFile.exists()){
+////                    Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+////                    ImageView imageView = (ImageView) findViewById(R.id.imageView2);
+////                    imageView.setImageBitmap(myBitmap);
+////                }
+//                //輸出圖片 方法二
+//                showBarcodeImage(result.getBarcodeImagePath());
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
+//    }
+//
+//    private void showBarcodeImage(String barcodeImagePath) {
+//        FileInputStream fileInputStream = null;
+//        try {
+//            fileInputStream = new FileInputStream(new File(barcodeImagePath));
+//            ((ImageView)findViewById(R.id.imageView2)).setImageBitmap(BitmapFactory.decodeStream(fileInputStream));
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                fileInputStream.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
